c9b13c79ea3a5d96b031b98c2e3cdf53bb46df53
eliseukadesh67 <eliseu.kadesh1967@gmail.com>
2021-05-19 08:21:20 -0300

    Arquivo de criaçao de banco de dados.

---
+++ create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE professors (
+	professor_id serial PRIMARY KEY UNIQUE,
+	nome VARCHAR ( 50 ) UNIQUE NOT NULL,
+	email VARCHAR ( 50 ) NOT NULL,
+	matricula VARCHAR ( 255 ) UNIQUE NOT NULL,
+  nascimento VARCHAR ( 255 ) UNIQUE NOT NULL
+);
+CREATE TABLE salas (
+  sala_id serial PRIMARY KEY,
+  numero INTEGER UNIQUE,
+  capacidade INTEGER,
+  disponivel BOOLEAN,
+);
+CREATE TABLE alunos (
+	aluno_id serial PRIMARY KEY,
+	nome VARCHAR ( 50 ) UNIQUE NOT NULL,
+	email VARCHAR ( 50 ) NOT NULL,
+	matricula VARCHAR ( 255 ) UNIQUE NOT NULL,
+  nascimento VARCHAR ( 255 ) UNIQUE NOT NULL,
+  sala INTEGER REFERENCES salas(numero)
+);
